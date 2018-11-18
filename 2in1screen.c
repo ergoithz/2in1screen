b0301ee906a3bebfdcb005a3a093b72103146bff
ergoithz <ergoithz@gmail.com>
2018-11-18 17:27:44 +0100

    switch to xcb for rotation

--- 2in1screen.c
+++ 2in1screen.c
@@ -1,7 +1,7 @@
 /*
  * Screen rotation daemon (preconfigured for Lenovo Thinkpad Yoga 11e)
  *
- * Compilation: gcc -O2 -o 2in1screen 2in1screen.c
+ * Compilation: gcc -O2 -o 2in1screen 2in1screen.c -lxcb -lxcb-randr -lxcb-xinput
  *
  * You will probably have to tune this stuff for your case:
  *   - TRANSFORM_PROPERTIES and TRANSFORM_PROPERTIES_SIZE
@@ -25,6 +25,12 @@ char
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
+#include <errno.h>
+
+#include <xcb/xcb.h>
+#include <xcb/randr.h>
+#include <xcb/xinput.h>
 
 /*
  * Tests (xyz for scale 0.019122):
@@ -43,9 +49,10 @@ double
   accel_y = 0.0,
   accel_z = 0.0;
 
-char
+unsigned char
   verbosity = 0,
-  current_state = 0;
+  current_state = 0,
+  keep_running = 1;
 
 #define DATA_SIZE 256
 char
@@ -73,6 +80,219 @@ char
     " 1  0  0  0  1 0 0 0 1",
     };
 
+#define CRTC_NAME "eDP1"
+#define CRTC_NAME_SIZE 4
+#define MIN(A, B) ((A) > (B) ? (B) : (A))
+
+int xcb_default_screen;
+xcb_randr_crtc_t xcb_randr_crtc;
+xcb_input_device_id_t xcb_input_device_id;
+xcb_window_t xcb_root;
+xcb_connection_t *xcb_connection = NULL;
+xcb_input_device_info_t *xcb_input_device_info = NULL;
+xcb_randr_screen_size_t *xcb_randr_screen_size = NULL;
+
+char xcb_detect_randr_crtc(const char *name, const int name_size){
+	xcb_randr_get_screen_resources_reply_t *screen_resources = xcb_randr_get_screen_resources_reply(
+    xcb_connection,
+    xcb_randr_get_screen_resources(xcb_connection, xcb_root),
+    NULL
+  );
+	if (screen_resources == NULL) return 1;
+
+  char success = 0;
+	int outputs_size = xcb_randr_get_screen_resources_outputs_length(screen_resources);
+	xcb_randr_output_t *outputs = xcb_randr_get_screen_resources_outputs(screen_resources);
+
+  xcb_randr_get_output_info_reply_t *output_info;
+  xcb_randr_crtc_t crtc;
+  xcb_randr_get_crtc_info_reply_t *crtc_info;
+	for (int i = 0; success == 0 && i < outputs_size; i++) {
+		output_info = xcb_randr_get_output_info_reply(
+      xcb_connection,
+      xcb_randr_get_output_info(xcb_connection, outputs[i], XCB_CURRENT_TIME),
+      NULL
+    );
+    if (output_info == NULL) continue;
+
+    crtc = output_info->crtc;
+    if(crtc == XCB_NONE) {
+      free(output_info);
+      continue;
+    }
+
+    crtc_info = xcb_randr_get_crtc_info_reply(
+      xcb_connection,
+      xcb_randr_get_crtc_info(xcb_connection, crtc, XCB_CURRENT_TIME),
+      NULL
+    );
+    if (crtc_info == NULL) {
+      free(output_info);
+      continue;
+    }
+
+    char *info_name = (char *) xcb_randr_get_output_info_name(output_info);
+    int info_name_size = xcb_randr_get_output_info_name_length(output_info);
+
+    if (strncmp(info_name, name, MIN(info_name_size, name_size)) == 0) {
+      xcb_randr_crtc = crtc;
+      success = 1;
+    }
+
+    free(output_info);
+    free(crtc_info);
+    // free(info_name);
+	}
+
+  free(screen_resources);
+  // free(outputs);
+  return success;
+}
+
+char xcb_init() {
+  // xcb_connection
+	xcb_connection = xcb_connect(NULL, &xcb_default_screen);
+	if (xcb_connection_has_error(xcb_connection)){
+		fprintf(stderr, "unable connect to the X server");
+		return EXIT_FAILURE;
+	}
+
+  // xcb_root
+	xcb_screen_t *xcb_screen = xcb_setup_roots_iterator(
+    xcb_get_setup(xcb_connection)
+  ).data;
+	if (xcb_screen == NULL){
+		fprintf(stderr, "unable to retrieve screen informations");
+		return EXIT_FAILURE;
+	}
+  xcb_root = xcb_screen->root;
+  // free(xcb_screen);
+
+  // xcb_randr_crtc
+  if (!xcb_detect_randr_crtc(CRTC_NAME, CRTC_NAME_SIZE)){
+		fprintf(stderr, "Failed to find crtc with name %s", CRTC_NAME);
+		return EXIT_FAILURE;
+  }
+
+  // xcb_randr_screen_size
+  xcb_randr_get_screen_info_reply_t *screen_info = xcb_randr_get_screen_info_reply(
+    xcb_connection,
+    xcb_randr_get_screen_info(xcb_connection, xcb_root),
+    NULL
+  );
+  if (screen_info == NULL){
+		fprintf(stderr, "unable to retrieve screen info");
+		return EXIT_FAILURE;
+	}
+	xcb_randr_screen_size = xcb_randr_get_screen_info_sizes(screen_info);
+  free(screen_info);
+
+	return EXIT_SUCCESS;
+}
+
+void xcb_close() {
+	if (xcb_connection != NULL) xcb_disconnect(xcb_connection);
+  if (xcb_input_device_info != NULL) free(xcb_input_device_info);
+  if (xcb_randr_screen_size != NULL) free(xcb_randr_screen_size);
+}
+
+int xcb_set_config(xcb_randr_crtc_t crtc, uint16_t rotation){
+	xcb_generic_error_t *e = NULL;
+	xcb_randr_get_crtc_info_reply_t *crtc_info = xcb_randr_get_crtc_info_reply(
+    xcb_connection,
+    xcb_randr_get_crtc_info(xcb_connection, crtc, XCB_CURRENT_TIME),
+    &e
+  );
+
+	if (e != NULL){
+		fprintf(stderr, "error get crtc info");
+		return EXIT_FAILURE;
+	}
+
+  int outputs_size = xcb_randr_get_crtc_info_outputs_length(crtc_info);
+	xcb_randr_output_t *outputs = xcb_randr_get_crtc_info_outputs(crtc_info);
+	xcb_randr_set_crtc_config_cookie_t cookie = xcb_randr_set_crtc_config(
+    xcb_connection,
+    crtc,
+    XCB_CURRENT_TIME,
+    XCB_CURRENT_TIME,
+    crtc_info->y,
+    crtc_info->x,
+    crtc_info->mode,
+    rotation,
+    outputs_size,
+    outputs
+  );
+
+	free(xcb_randr_set_crtc_config_reply(xcb_connection, cookie, &e));
+	free(crtc_info);
+
+	if (e != NULL){
+		fprintf(stderr, "error set crtc config");
+		return EXIT_FAILURE;
+	}
+
+	xcb_randr_get_screen_info_cookie_t screen_info_cookie =
+    xcb_randr_get_screen_info(xcb_connection, xcb_root);
+	xcb_randr_get_screen_info_reply_t *screen_info_reply =
+    xcb_randr_get_screen_info_reply(xcb_connection, screen_info_cookie, NULL);
+	xcb_randr_set_screen_config_cookie_t screen_config = xcb_randr_set_screen_config(
+    xcb_connection,
+    xcb_root,
+    XCB_CURRENT_TIME,
+    XCB_CURRENT_TIME,
+    screen_info_reply->sizeID,
+    rotation,
+    screen_info_reply->rate
+  );
+
+	free(xcb_randr_set_screen_config_reply(xcb_connection, screen_config, NULL));
+	free(screen_info_reply);
+	return EXIT_SUCCESS;
+}
+
+void xcb_rotate_screen() {
+  int rotation;
+  int width;
+  int height;
+  int mwidth;
+  int mheight;
+
+  switch (current_state){
+		case 1:
+      rotation = XCB_RANDR_ROTATION_ROTATE_180;
+      width = xcb_randr_screen_size->width;
+      height = xcb_randr_screen_size->height;
+      mwidth = xcb_randr_screen_size->mwidth;
+      mheight = xcb_randr_screen_size->mheight;
+			break;
+		case 2:
+			rotation = XCB_RANDR_ROTATION_ROTATE_90;
+      width = xcb_randr_screen_size->height;
+      height = xcb_randr_screen_size->width;
+      mwidth = xcb_randr_screen_size->mheight;
+      mheight = xcb_randr_screen_size->mwidth;
+			break;
+		case 3:
+      rotation = XCB_RANDR_ROTATION_ROTATE_270;
+      width = xcb_randr_screen_size->height;
+      height = xcb_randr_screen_size->width;
+      mwidth = xcb_randr_screen_size->mheight;
+      mheight = xcb_randr_screen_size->mwidth;
+			break;
+    default:
+      rotation = XCB_RANDR_ROTATION_ROTATE_0;
+      width = xcb_randr_screen_size->width;
+      height = xcb_randr_screen_size->height;
+      mwidth = xcb_randr_screen_size->mwidth;
+      mheight = xcb_randr_screen_size->mheight;
+	}
+
+  xcb_set_config(xcb_randr_crtc, rotation);
+  xcb_randr_set_screen_size(xcb_connection, xcb_root, width, height, mwidth, mheight);
+  xcb_flush(xcb_connection);
+}
+
 char rotation_changed() {
 
   // do not attempt to change orientation when horizontal
@@ -90,7 +310,7 @@ char rotation_changed() {
   return 1;
 }
 
-FILE* bdopen (char const *fname, char leave_open) {
+FILE* bdopen (char const *fname, unsigned char leave_open) {
   *basedir_end = '/';
   strcpy(basedir_end + 1, fname);
   FILE *fin = fopen(basedir, "r");
@@ -147,6 +367,12 @@ void help(char const *argv0) {
   );
 }
 
+void handle_signal(int sig) {
+  if (sig == SIGTERM || sig == SIGINT || sig == SIGHUP) {
+    keep_running = 0;
+  }
+}
+
 int main(int argc, char const *argv[]) {
   FILE *pf = popen("ls /sys/bus/iio/devices/iio:device*/in_accel*", "r");
   if (!pf) {
@@ -159,14 +385,14 @@ int main(int argc, char const *argv[]) {
     arg = argv[i];
 
     if (strcmp(arg, "--verbose") == 0) {
-    verbosity = 1;
+      verbosity = 1;
     } else if (strcmp(arg, "--help") == 0 || strcmp(arg, "--help") == 0) {
-    help(argv[0]);
-    return 0;
+      help(argv[0]);
+      return EXIT_SUCCESS;
     } else {
-    fprintf(stderr, "Invalid argument %s\n\n", arg);
-    help(argv[0]);
-    return 1;
+      fprintf(stderr, "Invalid argument %s\n\n", arg);
+      help(argv[0]);
+      return EXIT_FAILURE;
     }
   }
 
@@ -176,9 +402,13 @@ int main(int argc, char const *argv[]) {
     fprintf(stderr, "Accelerometer: %s\n", basedir);
   } else {
     fprintf(stderr, "Unable to find any accelerometer.\n");
-    return 1;
+    return EXIT_FAILURE;
   }
   pclose(pf);
+
+  signal(SIGTERM, handle_signal);
+	signal(SIGINT, handle_signal);
+  signal(SIGHUP, handle_signal);
 
   bdopen("in_accel_scale", 0);
   accel_scale = atof(content);
@@ -189,7 +419,9 @@ int main(int argc, char const *argv[]) {
   FILE *dev_accel_z = bdopen("in_accel_z_raw", 1);
   accel_z = atof(content) * accel_scale;
 
-  while (1) {
+  xcb_init();
+
+  while (keep_running) {
     if (verbosity) {
       printf(
         "Accelerometer x=%2f y=%2f z=%2f (%s)\n",
@@ -200,7 +432,8 @@ int main(int argc, char const *argv[]) {
         );
     }
     if (rotation_changed()) {
-      rotate_screen();
+      xcb_rotate_screen();
+      // rotate_screen();
     }
     sleep(2);
 
@@ -208,5 +441,8 @@ int main(int argc, char const *argv[]) {
     accel_y = read_dev_accel(dev_accel_y);
     accel_z = read_dev_accel(dev_accel_z);
   }
-  return 0;
+
+  xcb_close();
+
+  return EXIT_SUCCESS;
 }
